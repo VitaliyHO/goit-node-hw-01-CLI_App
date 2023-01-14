c378f2629bab22759433cdfce4074c43c901bb89
Гоцуляк Віталій <100048629+VitaliyHO@users.noreply.github.com>
2023-01-14 18:14:33 +0200

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+// // TODO: задокументувати кожну функцію
+
+async function fetchContacts() {
+  try {
+    const contactsArr = await fs.readFile(contactsPath);
+    const parsedContactsArr = JSON.parse(contactsArr);
+    return parsedContactsArr;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+async function listContacts() {
+  try {
+    const contactsArr = await fetchContacts();
+    console.table(contactsArr);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+async function getContactById(contactId) {
+  try {
+    const contactsArr = await fetchContacts();
+    const wantedContact = contactsArr.find((el) => el.id === contactId);
+    console.log(wantedContact);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+const addContact = async (name, email, phone) => {
+  const newContactData = {
+    name,
+    email,
+    phone,
+  };
+  try {
+    const contactsArr = await fetchContacts();
+    contactsArr.push(newContactData);
+    const updatedContactsArr = JSON.stringify(contactsArr, null, 4);
+    await fs.writeFile(contactsPath, updatedContactsArr);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
